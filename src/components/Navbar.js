3fd63bb6bf2ed7a0f74e79c68f0fc2f496376fac
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-03-09 14:20:30 +0000

    changes to file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,47 +10,47 @@ const Navbar = () => {
         @import
         url('https://fonts.googleapis.com/css2?family=Dosis&display=swap');
       </style>
-      <Link to="/">
-        <div className="logo">
+      <div className="logo">
+        <Link to="/">
           <img
             src={require("./logoTransparent.png")}
             alt="Logo"
             width="200"
             height="200"
           />
-        </div>
-      </Link>
+        </Link>
+      </div>
       <nav className="navBar">
-        <div className="navBarElements">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-              <ul className="dropdown">
-                <li>
-                  <a href="/">Bite of History</a>
-                </li>
-                <li>
-                  <a href="/">Back-end Project: Best Bytes</a>
-                </li>
-                <li>
-                  <a href="/">Front-end Project: SoundStage</a>
-                </li>
-                <li>
-                  <a href="/">Full-stack Project: Orbit</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link to="/skills">Skills</Link>
-            </li>
-          </ul>
-        </div>
+        {/* <div className="navBarElements"> */}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+            <ul className="dropdown">
+              <li>
+                <a href="/">Bite of History</a>
+              </li>
+              <li>
+                <a href="/">Back-end Project: Best Bytes</a>
+              </li>
+              <li>
+                <a href="/">Front-end Project: SoundStage</a>
+              </li>
+              <li>
+                <a href="/">Full-stack Project: Orbit</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to="/skills">Skills</Link>
+          </li>
+        </ul>
+        {/* </div> */}
       </nav>
     </div>
   );
