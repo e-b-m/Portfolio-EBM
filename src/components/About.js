c6aed1c68f6f3c4d6599a615fe07ce0230c2c502
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-03-13 11:16:11 +0000

    styling changes

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,11 @@ import React from "react";
 const About = () => {
   return (
     <section id="about">
+      <style className="font">
+        {" "}
+        @import
+        url('https://fonts.googleapis.com/css2?family=Dosis&display=swap');
+      </style>
       <div className="aboutContainer">
         <h1>Welcome to my portfolio!</h1>
       </div>
